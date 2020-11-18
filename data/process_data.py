ee025d1581fdca7fdc4c969b05dd6de1ed20d3f9
taha b uyar <54647308+tbuyar@users.noreply.github.com>
2020-11-18 02:25:47 -0500

    Update: Docstrings and clean_data were updated
    Missing docstrings were added and bad inputs were removed from the "related" category

--- data/process_data.py
+++ data/process_data.py
@@ -7,14 +7,37 @@ from sqlalchemy import create_engine
 import sqlite3
 
 def load_data(messages_filepath, categories_filepath):
-    
+    """ 
+    load data from csv file and return dataframe
+  
+    Parameters: 
+    messages_filepath (str): file path of messages.csv file
+    categories_filepath (str): file path of categories.csv file
+  
+    Returns: 
+    DataFrame: merged dataframe from two csv files
+  
+    """
+    # load messages and categories datasets
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
+    # merge datasets
     df = messages.merge(categories, on="id")
 
     return df
     
 def clean_data(df):
+    """ 
+    clean dataframe by several steps which includes splitting columns, converting dtypes, dropping duplicates
+  
+    Parameters: 
+    df (DataFrame): merged dataframe
+  
+    Returns: 
+    DataFrame: cleaned and organized dataframe
+  
+    """
+    # create a dataframe of the 36 individual category columns
     categories = df["categories"].str.split(expand=True, pat=";")
 
     # select the first row of the categories dataframe
@@ -42,10 +65,25 @@ def clean_data(df):
     
     # drop duplicates
     df.drop_duplicates(inplace=True)
+    
+    # drop the rows that related=2
+    df=df[df["related"]!=2]
    
     return df
     
 def save_data(df, database_filename):
+    """ 
+    save the clean dataset into an sqlite database
+  
+    Parameters: 
+    df (DataFrame): final dataframe
+    database_filename (str): name of database
+    
+    Returns: 
+    None
+  
+    """
+    # create engine and save it as SQL database by using pandas to_sql function
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('data', engine, index=False, if_exists='replace')
 
